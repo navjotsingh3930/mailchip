c126b951f46d04ea3d27b99e33ad3ce97e58bc84
navjotsingh3930 <131993694+navjotsingh3930@users.noreply.github.com>
2023-07-17 08:54:20 +0530

    inspiredContent Component Created

--- pages/why_mailchip.js
+++ pages/why_mailchip.js
@@ -3,12 +3,14 @@ import styles from "../styles/why_mailchip.module.css";
 import Banner from "@/components/why_mailchip/banner";
 import MarketingToolDesc from "@/components/why_mailchip/MarketingToolDesc";
 import InspiredSection from "@/components/why_mailchip/InspiredSection";
+import InspiredContent from "@/components/why_mailchip/InspiredContent";
 const why_mailchip = () => {
   return (
     <div className={styles.mainContainer}>
       <Banner />
       <MarketingToolDesc/>
       <InspiredSection/>
+      <InspiredContent/>
     </div>
   );
 };
